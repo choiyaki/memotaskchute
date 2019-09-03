2e8d9ec4cc96ce4b56bb06764e56c153ad50029b
Yahoo! <choiyaki@yahoo.com>
2019-09-04 08:23:13 +0900

    htmlを表示させたい

--- editor1/index.js
+++ editor1/index.js
@@ -14,3 +14,22 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('HellowWorld app is running on port', app.get('port'));
 });
+
+//１:モジュールのロード
+const http = require('http');
+const fs = require('fs');
+
+//２:サーバーオブジェクトの作成
+const server = http.createServer((req,res)=>{
+  //３:ファイル読み込み
+  fs.readFile('index.html','UTF-8',
+  (error, data)=>{
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write(data);
+    res.end();
+  });
+});
+
+//４:待ち受け開始
+server.listen(3000);
+console.log('Server running');
