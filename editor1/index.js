72840ebd032941af64a360ead3f147b01eccc04e
Yahoo! <choiyaki@yahoo.com>
2019-09-04 11:38:19 +0900

    わかりません

--- editor1/index.js
+++ editor1/index.js
@@ -1,38 +1,36 @@
 var express = require('express');
 var app = express();
 
-    //１:モジュールのロード
-const http = require('http');
-const fs = require('fs');
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', /*function(request, response) {
+app.get('/', function(req, res) {
+    fs.readFile('./lndex.html', 'utf-8', function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write('not found!');
+            return res.end();
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+});
+server.listen(ポート番号, ホスト名);
+console.log('server listening ...');
+
+
+/*function(request, response) {
   console.log("Hello World!!");
-  response.writeHead(200, {'Content-Type': 'text/plain'});*/
+  response.writeHead(200, {'Content-Type': 'text/plain'});
 
 
-//２:サーバーオブジェクトの作成
-const server = http.createServer((req,res)=>{
-console.log('Server beginning');
-  //３:ファイル読み込み
-  fs.readFile('index.html','UTF-8',
-  (error, data)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write(data);
-    res.end();
-  });
-});
-
-//４:待ち受け開始
-server.listen(3000);
-console.log('Server running');
-
-
-
-//response.end(/*'Hello heroku!!\n'*/);
+//response.end('Hello heroku!!\n');
   
-});
+});*/
 
 app.listen(app.get('port'), function() {
   console.log('HellowWorld app is running on port', app.get('port'));
